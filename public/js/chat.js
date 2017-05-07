bcb8afae71a63d653b9e097e04de497f46442f5d
JC De Guzman <JC De Guzman>
2017-05-07 17:22:42 +0800

    Send messages to only peopl in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -79,7 +79,6 @@ $('#message-form').submit(function(e) {
 
     if ($messageTextbox.val().trim() != '') {
         socket.emit('createMessage', {
-            from: 'User',
             text: $messageTextbox.val()
         }, function() {
             $messageTextbox.val('');
